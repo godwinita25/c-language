cb2ede4be10ba93d6eebf1a8c3e3f1f2ffc1dc63
godwinita25 <godwinita05@gmail.com>
2023-02-19 21:38:32 +0100

    my first hello world

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description - My first source code, to print hello world.
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("Hello, World! \n");
+
+	return (0);
+}
